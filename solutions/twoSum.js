a8af2d88c071fdf3695932a1aefc121878ef0c1e
Vandrosov99 <rpk633@gmail.com>
2020-05-08 18:22:43 +0300

    add description for two Sum + fix same pos

--- solutions/twoSum.js
+++ solutions/twoSum.js
@@ -1,7 +1,7 @@
 let twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j < nums.length; j++) {
-            if (target - nums[i] == nums[j]) {
+            if (target - nums[i] == nums[j] && (i != j)) {
                 return [i, j];
             }
         }
@@ -10,3 +10,13 @@ let twoSum = function (nums, target) {
 
 console.log(twoSum([1, 2, 5, 0, -5, 100], 5))
 
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example:
+
+// Given nums = [2, 7, 11, 15], target = 9,
+
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
