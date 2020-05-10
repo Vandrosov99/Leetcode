466e6cf46c93cda5a0030376825e4b1c97fc8530
Vandrosov99 <rpk633@gmail.com>
2020-05-10 16:32:18 +0300

    jewels and stones

---
+++ solutions/jews_stone.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function (J, S) {
+    let result = 0;
+
+    for (let i = 0; i < S.length; i++) {
+        for (let j = 0; j < J.length; j++) {
+            if (S[i] == J[j]) {
+                result++;
+            }
+        }
+    }
+    return result;
+};
+
+console.log(numJewelsInStones("aA", "aAAbbbb"))
